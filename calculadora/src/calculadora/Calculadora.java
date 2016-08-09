0b820cd0e88a06d2a18e37606a8ad921e3388b01
mrot2007 <mrot2007@gmail.com>
2016-08-09 20:00:52 -0300

    criação do projeto

---
+++ calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author marcelo
+ */
+public class Calculadora {
+
+    public double somar (double num1, double num2){
+    return 0.0;
+  }
+  public double subtrair (double num1, double num2){
+    return 0.0;
+  }
+  public double multiplicar (double num1, double num2){
+    return 0.0;
+  }
+  public double dividir (double num1, double num2){
+    return 0.0;
+  }
+  
+}
