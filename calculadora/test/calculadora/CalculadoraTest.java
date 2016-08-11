8c42d426bc2fe3d7216ebe3cd10060459c0be2b9
Marcelo <mrot2007@gmail.com>
2016-08-10 21:56:43 -0300

    inclusão classe de teste

---
+++ calculadora/test/calculadora/CalculadoraTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author marcelo
+ */
+public class CalculadoraTest {
+    
+    public CalculadoraTest() {
+    }
+
+    /**
+     * Test of somar method, of class Calculadora.
+     */
+    @Test
+    public void testSomar() {
+    }
+
+    /**
+     * Test of subtrair method, of class Calculadora.
+     */
+    @Test
+    public void testSubtrair() {
+    }
+
+    /**
+     * Test of multiplicar method, of class Calculadora.
+     */
+    @Test
+    public void testMultiplicar() {
+    }
+
+    /**
+     * Test of dividir method, of class Calculadora.
+     */
+    @Test
+    public void testDividir() {
+    }
+    
+}
